337d1574bad02822b226fec18ef41b1a82ee4e88
Nits PC <nitinrathod005@gmail.com>
2022-11-04 11:21:54 +0530

    showing current location on map

--- app/src/main/java/com/example/androidlocationtrakcer/MapsActivity.java
+++ app/src/main/java/com/example/androidlocationtrakcer/MapsActivity.java
@@ -1,7 +1,14 @@
 package com.example.androidlocationtrakcer;
 
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentActivity;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -16,6 +23,12 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     private GoogleMap mMap;
     private ActivityMapsBinding binding;
+    private LocationListener locationListener;
+    private LocationManager locationManager;
+    private LatLng latLng;
+
+    private final long MIN_TIME = 1000; // 1 second
+    private final long MIN_DIST = 5; // 5 meters
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +41,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, PackageManager.PERMISSION_GRANTED);
+        ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.ACCESS_COARSE_LOCATION}, PackageManager.PERMISSION_GRANTED);
     }
 
     /**
@@ -43,9 +59,26 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(@NonNull Location location) {
+                try {
+                    latLng = new LatLng(location.getLatitude(), location.getLongitude());
+                    mMap.addMarker(new MarkerOptions().position(latLng).title("My Position"));
+                    mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+                } catch (SecurityException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        };
+
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        try {
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, MIN_TIME, MIN_DIST, locationListener);
+        }
+        catch (SecurityException e) {
+            e.printStackTrace();
+        }
     }
 }
